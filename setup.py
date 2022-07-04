0c77fcc7d23298cdc03bd65d256092b3d4604f2e
borkusgod <lightraven.bourque@gmail.com>
2022-07-04 07:01:26 -0300

    This is the initial project state. There will be many revisions and forks to come. Stay tune!

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='RandomPyCharGen',
+    version='0.0.1',
+    packages=['virtual', 'virtual.RandomCharGen'],
+    url='https://github.com/borkusgod',
+    license='LPGL',
+    author='William Bourque',
+    author_email='',
+    description='A simple python program that creates a string of random '
+                'characters and places them into a data collection of your '
+                'choice. '
+)
